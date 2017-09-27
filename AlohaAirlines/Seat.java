572668c1dee1ce4ba1215ee11800487bbd5f433f
Evan Kohout <ebox8_000@DESKTOP-PC.(none)>
2017-09-27 10:02:55 -0700

    added more to Seat class

--- AlohaAirlines/Seat.java
+++ AlohaAirlines/Seat.java
@@ -2,17 +2,28 @@ package AlohaAirlines;
 
 public class Seat {
 
-	private final String id;
+	private String seatId;
 	private int row;
 	private int col;
 	private double price;
 	private int recline;
 	private String meal;
+	private String confirmationId;
 	
-	public void Seat(int row, int col, double price, int recline, String meal){
+	public Seat(int row, int col, double price, int recline, String meal){
 		if(price < 0.0) return;
 		if(row < 0 || row > 40) return;
 		if(col < 0 || col > 6) return;
 		if(recline < 0) return;
+		this.row = row;
+		this.col = col;
+		seatId = row + getCharForNum(col);
+		this.price = price;
+		this.meal = meal;
+	}
+	
+	private String getCharForNum(int i) {
+	    char[] alphabet = "ABCDEF".toCharArray();
+	    return Character.toString(alphabet[i]);
 	}
 }
